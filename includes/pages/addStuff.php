41f277aec840d9644273c77ca3d9ec62defa051b
theHobbyistGit <the.hobbyist@gmx.de>
2015-09-11 14:06:42 +0200

    added first part of charactersheet and added a bit styling

--- includes/pages/addStuff.php
+++ includes/pages/addStuff.php
@@ -2,7 +2,13 @@
 <div class="normalArea">
   <p>Bitte wähle aus was für einen Charakterbogen du erstellen möchtest: </p>
   <select id="chooseEmptyCharSheet" name="chooseEmptyCharSheet">
-    <option value="0">V20 Vampir Bogen</option>
+    <option value="0">V20 Vampir Bogen (Spieler)</option>
+    <option value="1">V20 Vampir Bogen (NSC)</option>
   </select>
   <button id="chooseEmptyCharSheetButton" type="button">Auswählen</button>
+</div>
+<div class="normalArea">
+  <form id="createCharacter" name="createCharacter">
+    <?php require("includes/sheets/v20VampirePlayerSheet.php") ?>
+  </form>
 </div>
---
+++ includes/sheets/v20VampirePlayerSheet.php
@@ -0,0 +1,48 @@
+<table id="characterSheet">
+  <tr>
+    <td><label>Name: </label><input type="text" name="charName"/></td>
+    <td><label>Wesen:</label><input type="text" name="character" /></td>
+    <td><label>Clan: </label><input type="text" name="clan"/></td>
+  </tr>
+  <tr>
+    <td><label>Spieler:   </label><input type="text" name="playerName"/></td>
+    <td><label>Verhalten: </label><input type="text" name="behaviour"/></td>
+    <td><label>Generation:</label><input type="text" name="generation"/></td>
+  </tr>
+  <tr>
+    <td><label>Chronik: </label><input type="text" name="chronicle"/></td>
+    <td><label>Konzept: </label><input type="text" name="concept"/></td>
+    <td><label>Erzeuger:</label><input type="text" name="sire"/></td>
+  </tr>
+
+  <tr class="tableHorizontalSpacer"><td colspan="3">ATTRIBUTE</td></tr>
+
+  <tr><td>Körperkraft<span></span></td><td>Charisma<span></span></td><td>Wahrnehmung<span></span></td></tr>
+  <tr><td>Geschick<span></span></td><td>Manipulation<span></span></td><td>Intelligenz<span></span></td></tr>
+  <tr><td>Widerstandsfähigkeit<span></span></td><td>Erscheinungsbild<span></span></td><td>Geistesschärfe<span></span></td></tr>
+
+  <tr class="tableHorizontalSpacer"><td colspan="3">FÄHIGKEITEN</td></tr>
+
+  <tr><td>Aufmerksamkeit<span></span></td><td>Diebstahl<span></span></td><td>Akademisches Wissen<span></span></td></tr>
+  <tr><td>Ausdruck<span></span></td><td>Etikette<span></span></td><td>Computer<span></span></td></tr>
+  <tr><td>Ausflüchte<span></span></td><td>Fahren<span></span></td><td>Finanzen<span></span></td></tr>
+  <tr><td>Einschüchtern<span></span></td><td>Handwerk<span></span></td><td>Gesetzeskenntnis<span></span></td></tr>
+  <tr><td>Empathie<span></span></td><td>Heimlichkeit<span></span></td><td>Medizin<span></span></td></tr>
+  <tr><td>Führungsqualitäten<span></span></td><td>Nahkampf<span></span></td><td>Nachforschungen<span></span></td></tr>
+  <tr><td>Handgemenge<span></span></td><td>Schusswaffen<span></span></td><td>Naturwissenschaften<span></span></td></tr>
+  <tr><td>Magiegespür<span></span></td><td>Tierkunde<span></span></td><td>Okkultismus<span></span></td></tr>
+  <tr><td>Sportlichkeit<span></span></td><td>Überleben<span></span></td><td>Politik<span></span></td></tr>
+  <tr><td>Szenekenntnis<span></span></td><td>Vortrag<span></span></td><td>Technologie<span></span></td></tr>
+
+  <tr class="tableHorizontalSpacer"><td colspan="3">VORTEILE</td></tr>
+
+  <tr><td><span></span></td><td><span></span></td><td><span></span></td></tr>
+  <tr><td><span></span></td><td><span></span></td><td><span></span></td></tr>
+  <tr><td><span></span></td><td><span></span></td><td><span></span></td></tr>
+  <tr><td><span></span></td><td><span></span></td><td><span></span></td></tr>
+  <tr><td><span></span></td><td><span></span></td><td><span></span></td></tr>
+  <tr><td><span></span></td><td><span></span></td><td><span></span></td></tr>
+
+  <tr class="tableHorizontalSpacer"><td colspan="3"></td></tr>
+
+</table>
