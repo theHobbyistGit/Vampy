ace0b930d31055ce1669ef2b6fc30ed7cb34c543
theHobbyistGit <the.hobbyist@gmx.de>
2015-09-11 15:14:16 +0200

    Made the first sheet bigger and added the fundamental table structure

--- includes/sheets/v20VampirePlayerSheet.php
+++ includes/sheets/v20VampirePlayerSheet.php
@@ -17,32 +17,79 @@
 
   <tr class="tableHorizontalSpacer"><td colspan="3">ATTRIBUTE</td></tr>
 
-  <tr><td>Körperkraft<span></span></td><td>Charisma<span></span></td><td>Wahrnehmung<span></span></td></tr>
-  <tr><td>Geschick<span></span></td><td>Manipulation<span></span></td><td>Intelligenz<span></span></td></tr>
-  <tr><td>Widerstandsfähigkeit<span></span></td><td>Erscheinungsbild<span></span></td><td>Geistesschärfe<span></span></td></tr>
+  <tr><th>Körperlich</th><th>Gesellschaftlich</th><th>Geistig</th></tr>
+  <tr><td>Körperkraft<span>&#9679;&#9675;&#9675;&#9675;&#9675;</span></td><td>Charisma<span>&#9679;&#9675;&#9675;&#9675;&#9675;</span></td><td>Wahrnehmung<span>&#9679;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
+  <tr><td>Geschick<span>&#9679;&#9675;&#9675;&#9675;&#9675;</span></td><td>Manipulation<span>&#9679;&#9675;&#9675;&#9675;&#9675;</span></td><td>Intelligenz<span>&#9679;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
+  <tr><td>Widerstandsfähigkeit<span>&#9679;&#9675;&#9675;&#9675;&#9675;</span></td><td>Erscheinungsbild<span>&#9679;&#9675;&#9675;&#9675;&#9675;</span></td><td>Geistesschärfe<span>&#9679;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
 
   <tr class="tableHorizontalSpacer"><td colspan="3">FÄHIGKEITEN</td></tr>
 
-  <tr><td>Aufmerksamkeit<span></span></td><td>Diebstahl<span></span></td><td>Akademisches Wissen<span></span></td></tr>
-  <tr><td>Ausdruck<span></span></td><td>Etikette<span></span></td><td>Computer<span></span></td></tr>
-  <tr><td>Ausflüchte<span></span></td><td>Fahren<span></span></td><td>Finanzen<span></span></td></tr>
-  <tr><td>Einschüchtern<span></span></td><td>Handwerk<span></span></td><td>Gesetzeskenntnis<span></span></td></tr>
-  <tr><td>Empathie<span></span></td><td>Heimlichkeit<span></span></td><td>Medizin<span></span></td></tr>
-  <tr><td>Führungsqualitäten<span></span></td><td>Nahkampf<span></span></td><td>Nachforschungen<span></span></td></tr>
-  <tr><td>Handgemenge<span></span></td><td>Schusswaffen<span></span></td><td>Naturwissenschaften<span></span></td></tr>
-  <tr><td>Magiegespür<span></span></td><td>Tierkunde<span></span></td><td>Okkultismus<span></span></td></tr>
-  <tr><td>Sportlichkeit<span></span></td><td>Überleben<span></span></td><td>Politik<span></span></td></tr>
-  <tr><td>Szenekenntnis<span></span></td><td>Vortrag<span></span></td><td>Technologie<span></span></td></tr>
+  <tr><th>Talente</th><th>Fähigkeiten</th><th>Kenntnisse</th></tr>
+  <tr><td>Aufmerksamkeit<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Diebstahl<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Akademisches Wissen<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
+  <tr><td>Ausdruck<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Etikette<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Computer<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
+  <tr><td>Ausflüchte<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Fahren<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Finanzen<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
+  <tr><td>Einschüchtern<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Handwerk<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Gesetzeskenntnis<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
+  <tr><td>Empathie<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Heimlichkeit<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Medizin<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
+  <tr><td>Führungsqualitäten<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Nahkampf<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Nachforschungen<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
+  <tr><td>Handgemenge<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Schusswaffen<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Naturwissenschaften<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
+  <tr><td>Magiegespür<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Tierkunde<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Okkultismus<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
+  <tr><td>Sportlichkeit<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Überleben<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Politik<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
+  <tr><td>Szenekenntnis<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Vortrag<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Technologie<span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
 
   <tr class="tableHorizontalSpacer"><td colspan="3">VORTEILE</td></tr>
 
-  <tr><td><span></span></td><td><span></span></td><td><span></span></td></tr>
-  <tr><td><span></span></td><td><span></span></td><td><span></span></td></tr>
-  <tr><td><span></span></td><td><span></span></td><td><span></span></td></tr>
-  <tr><td><span></span></td><td><span></span></td><td><span></span></td></tr>
-  <tr><td><span></span></td><td><span></span></td><td><span></span></td></tr>
-  <tr><td><span></span></td><td><span></span></td><td><span></span></td></tr>
+  <tr><th>Disziplinen</th><th>Hintergründe</th><th>Tugenden</th></tr>
+  <tr><td><span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td><span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Gewissen/</td></tr>
+  <tr><td><span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td><span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Überzeugung<span>&#9679;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
+  <tr><td><span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td><span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Selbstbeherrschung/</td></tr>
+  <tr><td><span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td><span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Instinkt<span>&#9679;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
+  <tr><td><span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td><span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td></td></tr>
+  <tr><td><span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td><span>&#9675;&#9675;&#9675;&#9675;&#9675;</span></td><td>Mut<span>&#9679;&#9675;&#9675;&#9675;&#9675;</span></td></tr>
 
   <tr class="tableHorizontalSpacer"><td colspan="3"></td></tr>
+
+  <tr><th>Vorzüge & Schwächen</th><th>Menschlichkeit/Pfad</th><th>Gesundheit</th></tr>
+  <tr>
+    <td>
+      <table>
+        <tr><th>Vorzug</th><th>Kosten</th></tr>
+        <tr><td></td><td></td></tr>
+        <tr><td></td><td></td></tr>
+        <tr><td></td><td></td></tr>
+        <tr><td></td><td></td></tr>
+        <tr><td></td><td></td></tr>
+        <tr><th>Schwäche</th><th>Kosten</th></tr>
+        <tr><td></td><td></td></tr>
+        <tr><td></td><td></td></tr>
+        <tr><td></td><td></td></tr>
+        <tr><td></td><td></td></tr>
+        <tr><td></td><td></td></tr>
+      </table>
+    </td>
+    <td>
+      <table>
+        <tr><td></td></tr>
+        <tr><td>&#9675;&#9675;&#9675;&#9675;&#9675;&#9675;&#9675;&#9675;&#9675;&#9675;</td></tr>
+        <tr><td>Haltung:<span>(   )</span></td></tr>
+        <tr><th>Willenskraft</th></tr>
+        <tr><td>&#9675;&#9675;&#9675;&#9675;&#9675;&#9675;&#9675;&#9675;&#9675;&#9675;</td></tr>
+        <tr><td>&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;</td></tr>
+        <tr><th>Blutvorrat</th></tr>
+        <tr><td>&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;</td></tr>
+        <tr><td>&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;&#9725;</td></tr>
+      </table>
+    </td>
+    <td>
+      <table>
+        <tr><td>Blaue Flecken   </td><td>  </td><td>&#9725;</td></tr>
+        <tr><td>Verletzt        </td><td>-1</td><td>&#9725;</td></tr>
+        <tr><td>Schwer verletzt </td><td>-1</td><td>&#9725;</td></tr>
+        <tr><td>Verwundet       </td><td>-2</td><td>&#9725;</td></tr>
+        <tr><td>Schwer verwundet</td><td>-2</td><td>&#9725;</td></tr>
+        <tr><td>Verkrüppelt     </td><td>-5</td><td>&#9725;</td></tr>
+        <tr><td>Außer Gefecht   </td><td>  </td><td>&#9725;</td></tr>
+      </table>
+    </td>
+  </tr>
 
 </table>
